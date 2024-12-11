f9fff79f28755702cfc89ccc5698f4369c6e3a73
Bogdan Tudosie <bogdan.tudosie@withsecure.com>
2024-12-11 09:48:56 +0200

    Initial Commit

---
+++ BundaToDoTests/BundaToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  BundaToDoTests.swift
+//  BundaToDoTests
+//
+//  Created by Bogdan Tudosie on 11.12.2024.
+//
+
+import Testing
+@testable import BundaToDo
+
+struct BundaToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
